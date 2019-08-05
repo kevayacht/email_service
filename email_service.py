dd77d1927fdbaf9e04a8d760f123991100444c25
Kevin Smith <kevinsmith.kis@gmail.com>
2019-08-05 21:52:45 +0200

    Preliminary class construct
    Looking into a class construct to manage the insertion of data to the email service. Looking into use of __dict__.

--- email_service.py
+++ email_service.py
@@ -1,6 +1,23 @@
+from datetime import datetime, timedelta
+
+
+class EmailService:
+
+    def __init__(self):
+        self.current_date = datetime.now().date()
+    def load_details(self):
+        pass
+
 
 def main():
-    pass
+    email_details = {   "host": "",
+                        "port": "",
+                        "host_user": "",
+                        "host_password": "",
+                        "sender": "",
+                        "subject":"",
+                        "message":"",
+                        }
 
 if __name__ == '__main__':
     main()
