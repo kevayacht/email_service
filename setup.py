d8e656ee67ca528ba5c75e712e040bb00bd76e1c
Kevin Smith <kevinsmith.kis@gmail.com>
2019-08-11 13:10:12 +0200

    created setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='email_service',
+   version='1.0',
+   description='A emailing service',
+   author='Kevin Smith',
+   author_email='kevinsmith.kis@gmail.com',
+   packages=['email_service'],
+   install_requires=[],
+)
